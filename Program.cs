95a2dbeda4d3c2125f0534dd8b938421fa0360b7
Daryl Fong <darkare@gmail.com>
2025-09-19 09:38:12 +1000

    three

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace console1
         {
             Console.WriteLine("Hello World One!");
             Console.WriteLine("Hello World Two Two!");
+            Console.WriteLine("Hello World Three!");
         }
     }
 }
